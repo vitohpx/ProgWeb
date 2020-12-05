8fc2c35d8c306a7ef79f858f8ae7246b669d5803
vitor <vitor@.lepidus.com.br>
2020-12-05 13:28:12 -0400

    Adicionando o ORM, BD, models, migrations, tabelas e seeders

--- Game/config/router.js
+++ Game/config/router.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const main = require("../app/controllers/main");
-const mainController = require("../app/controllers/main")
+const mainController = require("../app/controllers/main");
+const areaController = require("../app/controllers/area");
 const router = express.Router();
 
 router.get("/", mainController.index);
-
 router.get("/sobre", mainController.sobre);
-
 router.get("/game",mainController.game);
-
 router.get("/ui",mainController.ui);
+
+router.get("/area", areaController.index);
 
 module.exports = router;
